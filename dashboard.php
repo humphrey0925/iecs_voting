97bcd9f65215598659061887bc336b36415f6000
柯博瀚 <kebohan1@gmail.com>
2017-06-08 06:50:17 +0800

    no message

--- dashboard.php
+++ dashboard.php
@@ -3,11 +3,11 @@
 <head>
 	<title>逢甲大學資訊一甲幹部投票系統</title>
 </head>
+<link rel=stylesheet type="text/css" href="dashboard.css">
 <script type="text/javascript">
 	
 </script>
 <?php 
-	echo "hi";
 		if(!isset($_COOKIE["login"]))
 		{
         	header("Location: index.php"); //將網址改為要導入的登入頁面
@@ -16,44 +16,59 @@
 		else
 		{
 			
-		define('DB_HOST', 'nihao.dynu.net:33306');
-	    define('DB_USER', 'classvote');
-	    define('DB_PASS', 'classvote');
-	    define('DB_NAME', 'zi_xun_yi_jia_vote');
-	    $password = crypt($unpassword, '$6$IECS$' . $unpassword . '$');
-		$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-	    $query = "SELECT count(`password`) FROM `user` WHERE `account` = '$account' AND `password` = '$password';";
-	    $list=mysqli_query($dbc, $query);
-	    $row=mysqli_fetch_row($list);
+			define('DB_HOST', 'nihao.dynu.net:33306');
+		    define('DB_USER', 'classvote');
+		    define('DB_PASS', 'classvote');
+		    define('DB_NAME', 'zi_xun_yi_jia_vote');
+			$dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+		    $query = "SELECT * FROM ALL_VOTE WHERE `live` = 1";
+		    $list=mysqli_query($dbc, $query);
+		    if($list)
+		    {
+		    	
+	    
+	    
     ?>
 <body>
 	<div>
-		<div>
+		<div id="header">
 			<div>
 				
 			</div>
 		</div>
-		<div>
+		<div id="imp_part">
 			<div id="navigate_bar">
 				<div id="login_status">
 					<p>用戶<?php ?>您好，歡迎您的登陸</p>
+					<div id="logout">
+						登出
+					</div>
 				</div>
-				<div>
-					功能導覽
+				<div id="feature">
+					<p id="feature_string">功能導覽</p>
 					<h2>投票功能</h2>
 					<ul>
 						<?php
+							for($i=1;$i<=mysqli_num_rows($list);$i++)
+							{
+								$row=mysqli_fetch_row($list);
+								?><li><?php echo $row[0];?></li><?php
+							}
 						?>
 					</ul>
 					<h2>個人資料設定</h2>
+					<ul>
+						<li>設定密碼</li>
+					</ul>
 				</div>
 			</div>
-			<div>
+			<div id="control_page">
 								
 			</div>
 		</div>
 	</div>
     <?php
+			}	
 		}
 	?>
 </body>
